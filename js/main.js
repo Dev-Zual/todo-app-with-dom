375b92ba1ea606143cb32f5f8c61b77450bc363e
Zual Rana <freelancerzual@gmail.com>
2023-04-10 12:22:42 +0600

    delete & done btn work

--- js/main.js
+++ js/main.js
@@ -9,13 +9,25 @@ document.getElementById("button-addon2").addEventListener("click", function () {
   const tableBody = document.getElementById("table-body");
 
   const tr = document.createElement("tr");
+  tr.classList.add("tr-container");
   tr.innerHTML = `
     <th scope="row">${count}</th>
     <td>${inputText}</td>
-    <td><button class="btn btn-danger btn-delete me-1">Delete</button><button class="btn btn-primary">Done</button></td>
+    <td><button class="btn btn-danger btn-delete me-1">Delete</button><button class="btn btn-primary btn-done">Done</button></td>
   `;
-
   tableBody.appendChild(tr);
+  document.getElementById("text-field").value = "";
+  const deleteBtn = document.getElementsByClassName("btn-delete");
+  for (const btn of deleteBtn) {
+    btn.addEventListener("click", function (e) {
+      e.target.parentNode.parentNode.style.display = "none";
+    });
+  }
+  const doneBtn = document.getElementsByClassName("btn-done");
 
-  console.log(inputText);
+  for (const btn of doneBtn) {
+    btn.addEventListener("click", function () {
+      btn.parentNode.parentNode.style.textDecoration = "line-through";
+    });
+  }
 });
